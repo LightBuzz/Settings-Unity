118527d150dbe99c0e4a21ac9fb49cbca132809d
Vangos Pterneas <vangos@lightbuzz.com>
2018-07-26 15:09:39 +0300

    Updated the sample project.

---
+++ LightBuzz.Settings.Unity.Demo/Assets/LightBuzz_Settings/Sample/LightBuzz_Settings_Sample.cs
@@ -0,0 +1,27 @@
+﻿using LightBuzz.Settings;
+using System;
+using UnityEngine;
+
+public class LightBuzz_Settings_Sample : MonoBehaviour
+{
+    private void Start()
+    {
+        Settings.Initialize();
+    }
+
+    public void Set_Click()
+    {
+        DateTime date = DateTime.Now;
+
+        Settings.Set("lightbuzz_date", date);
+
+        Debug.Log("Saved!");
+    }
+
+    public void Get_Click()
+    {
+        DateTime date = Settings.Get<DateTime>("lightbuzz_date");
+
+        Debug.Log(date);
+    }
+}
